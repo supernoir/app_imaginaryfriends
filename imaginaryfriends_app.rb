7de7306858250bee86bc690e930f212409f300d3
supernoir <kawohl.bentodesign@gmail.com>
2015-07-22 23:50:45 +0200

    Got the write to JSON and read from JSON to work... yay!

--- imaginaryfriends_app.rb
+++ imaginaryfriends_app.rb
@@ -21,6 +21,8 @@ post '/create_character' do
   $age = params[:age]
   $origin = params[:origin]  
 
+  assemble_characters
+
   @newfirstname = $first_name
   @newlastname = $last_name
   @newage = $age
@@ -29,18 +31,24 @@ post '/create_character' do
   erb :new_character
 end
 
-
-def add_new_characters 
-  #newfirstname = yield
+# Assembles a single character with the data supplied by the user
+def assemble_characters 
   $character = {"First Name" => $first_name, "Last Name" => $last_name, "Age" => $age, "Origin" => $origin}
+  write_character($character)
 end
-add_new_characters
 
-def write_character($character)
+# Read the JSON File and parse it into a hash
+def read_characters()
+  readfile = File.read("characters.json")
+  return JSON.parse(readfile)
+end
+
+# Reads the Characters JSON file and appends the new character
+def write_character(character)
+  
   characters = read_characters()
   characters << $character
   f = File.open("characters.json", "w") 
   f << characters.to_json
   f.close
 end
-write_character
